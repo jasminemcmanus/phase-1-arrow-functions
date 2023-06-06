17c487509da030554996bf92d358227a0d988558
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-06-06 13:34:44 +1000

    done with assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// divide function
+const divide = function(a,b) {
+    return a / b;
+}
+console.log(divide(100,2));
+
+// square function
+const square = (a) => a * a
+console.log(square(5));
+
+// add function
+const add = (a, b) => a + b
+console.log(add(2,3));
